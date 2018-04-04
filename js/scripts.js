bda5c2a1e5e1d34f3b510507981fdff689683f75
Michał Brusiło <mibru@interia.pl>
2018-04-04 15:07:25 +0200

    Add index.html and scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// instrukcje warunkowe w praktyce
+var a = -3;
+var b = 65;
+var value;
+
+value = (a * a) - (2 * a * b) + (b * b);
+
+console.log(value);
+
+if (value >0) {
+	console.log("wynik dodatni");
+} else if (value <0) {
+	colnsole.log("Wynik ujemny");
+}
+else {
+	console.log("Wynik wynosi zero");
+}
